ef7769536b6c45f11f174b89c84d914ebdbf39a5
Aryaman Srivastava <aryamansrivastava895@gmail.com>
2022-11-16 20:41:13 +0530

    munch: Initial ReloadedOS 13.0 bringup

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 Reloaded OS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/munch.mk
+
+COMMON_LUNCH_CHOICES := \
+    munch-user \
+    munch-userdebug \
+    munch-eng
