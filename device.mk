00ffbfdc97a11acba1dbf88874f0db01df481ed8
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-02-05 16:22:03 +0530

    munch: overlay: Switch to vendor-defined color modes
     * vivid (default), saturated, original, P3 and sRGB
     * according to miui

--- device.mk
+++ device.mk
@@ -154,6 +154,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
 
 PRODUCT_VENDOR_PROPERTIES += \
     debug.sf.enable_hwc_vds=1 \
+    persist.sys.sf.native_mode=258 \
     ro.surface_flinger.set_idle_timer_ms=1000 \
     ro.surface_flinger.set_touch_timer_ms=1000 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
