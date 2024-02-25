86ecd3f6d42cd3dc486a2cf55e0e45bd8423b59f
madmax7896 <madmax78968@gmail.com>
2024-02-25 19:55:26 +0530

    munch: Move IRQ config to qcom/common

--- device.mk
+++ device.mk
@@ -299,10 +299,6 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Perf
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # Platform
 TARGET_BOARD_PLATFORM := kona
 
