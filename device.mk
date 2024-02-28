c3b91e96e9d63a7a77ae19828b0b283d1b1ce059
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-02-28 20:31:19 +0530

    munch: Build Xiaomi sensors impl
    Change-Id: Ib477ac016c312ce02e9f71df2a7d1d0fcca717e8

--- device.mk
+++ device.mk
@@ -339,7 +339,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl-xiaomi \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
